275a06d10ef735079261ba3d3718c5e5caac0bea
adminsishuhanhan <1970745691@qq.com>
2026-09-15 00:47:04 +0800

    Update djmusic.js

--- djmusic.js
+++ djmusic.js
@@ -1,1 +1,17 @@
-let test = await request('https://www.baidu.com');
+一级: async function () {
+    return [
+        { type_id: 'test', type_name: '测试分类' }
+    ];
+},
+
+二级: async function (tid, pg, filter, ext) {
+    if (tid === 'test') {
+        return [{
+            vod_id: '1',
+            vod_name: '测试成功',
+            vod_pic: '',
+            vod_remarks: ''
+        }];
+    }
+    // ...正常逻辑
+}
