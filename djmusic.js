b1add86a18d9b9855d402c0d0ff9974fa6d080f8
adminsishuhanhan <1970745691@qq.com>
2026-09-15 01:00:33 +0800

    Update djmusic.js

--- djmusic.js
+++ djmusic.js
@@ -1,99 +1,30 @@
-var rule = {
-    title: 'DJ音乐网',
-    host: 'https://www.dj.net',
-    homeUrl: '/dj-class-84-1.html',
-    searchUrl: '/search.php?mod=music&srchtxt=**&searchsubmit=yes',
-    searchable: 2,
-    quickSearch: 1,
-    filterable: 0,
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36',
-        'Referer': 'https://www.dj.net/'
-    },
-    timeout: 8000,
-
-    // ========== 分类列表 ==========
-    一级: async function () {
-        return [
-            { type_id: '13', type_name: '慢摇串烧' },
-            { type_id: '80', type_name: '酒吧串烧' },
-            { type_id: '82', type_name: '专业串烧' },
-            { type_id: '5',  type_name: '歌曲连版' },
-            { type_id: '17', type_name: '外文单曲' },
-            { type_id: '84', type_name: '中文单曲' },
-            { type_id: '85', type_name: 'Electro' },
-            { type_id: '86', type_name: 'House' },
-            { type_id: '87', type_name: 'Deep' },
-            { type_id: '88', type_name: 'Breakbeat' },
-            { type_id: '89', type_name: 'Club' },
-            { type_id: '90', type_name: 'EDM' },
-            { type_id: '91', type_name: 'Mashup' },
-            { type_id: '92', type_name: 'Rab' },
-            { type_id: '93', type_name: 'Funky' },
-            { type_id: '94', type_name: '流行音乐' }
-        ];
-    },
-
-    // ========== 分类内容 ==========
-    二级: async function (tid, pg, filter, ext) {
-        let url = rule.host + '/dj-class-' + tid + '-' + pg + '.html';
-        let html = await request(url, {
-            headers: rule.headers,
-            encoding: 'gbk'
-        });
-        return parseList(html);
-    },
-
-    // ========== 搜索 ==========
-    搜索: async function (wd, pg) {
-        let url = rule.host + '/search.php?mod=music&srchtxt=' + encodeURIComponent(wd) + '&searchsubmit=yes';
-        let html = await request(url, {
-            headers: rule.headers,
-            encoding: 'gbk'
-        });
-        return parseList(html);
-    },
-
-    // ========== 详情 ==========
-    详情: async function (id) {
-        return {
-            vod_id: id,
-            vod_name: '',
-            vod_pic: '',
-            vod_content: '',
-            vod_play_from: 'DJ音乐',
-            vod_play_url: '播放$' + id
-        };
-    },
-
-    // ========== 播放（核心）==========
-    播放: async function (flag, id, flags) {
-        let apiUrl = rule.host + '/template/zhzh_dzmusic/ajax/?action=geturl';
-        let res = await post(apiUrl, { id: id }, {
-            headers: rule.headers,
-            encoding: 'gbk'
-        });
-        let json = JSON.parse(res);
-        if (json.error !== '0' || !json.data || json.data.length === 0) {
-            return '获取失败';
-        }
-        let d = json.data[0];
-        return d.ser[0].u + d.src;
-    }
-};
-
-// ========== 通用列表解析 ==========
-function parseList(html) {
-    let items = [];
-    let reg = /<a href="https:\/\/www\.dj\.net\/djplay\/music(\d+)\.html"[^>]*title="([^"]+)"/g;
-    let m;
-    while ((m = reg.exec(html)) !== null) {
-        items.push({
-            vod_id: m[1],
-            vod_name: m[2],
-            vod_pic: '',
-            vod_remarks: ''
-        });
-    }
-    return items;
+{
+    "作者": "自定义",
+    "站名": "DJ音乐网",
+    "编码": "GBK",
+    "请求头": "User-Agent$MOBILE_UA",
+    "主页url": "https://www.dj.net",
+    "首页": "200",
+    "起始页": "1",
+    "分类url": "/dj-class-{cateId}-{catePg}.html",
+    "分类": "慢摇串烧$13#酒吧串烧$80#专业串烧$82#歌曲连版$5#外文单曲$17#中文单曲$84#Electro$85#House$86#Deep$87#Breakbeat$88#Club$89#EDM$90#Mashup$91#Rab$92#Funky$93#流行音乐$94",
+    "分类二次截取": "",
+    "分类数组": "",
+    "分类标题": "",
+    "分类ID": "",
+    "二次截取": "",
+    "数组": "<a href=\"https://www.dj.net/djplay/music&&</a>",
+    "图片": "",
+    "标题": "title=\"&&\"",
+    "副标题": "",
+    "链接": "music&&.html",
+    "简介": "",
+    "搜索url": "/search.php?mod=music&srchtxt={wd}&searchsubmit=yes",
+    "搜索数组": "<a href=\"https://www.dj.net/djplay/music&&</a>",
+    "搜索标题": "title=\"&&\"",
+    "搜索图片": "",
+    "搜索副标题": "",
+    "搜索链接": "music&&.html",
+    "直接播放": "https://www.dj.net/template/zhzh_dzmusic/ajax/?action=geturl&id={id}",
+    "播放头": ""
 }
